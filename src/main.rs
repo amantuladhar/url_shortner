8afe9bac1fd92937c2c115d5a944092edff6ad3b
Aman Tuladhar <atuladhar.aman@gmail.com>
2022-11-06 01:08:25 -0500

    Simple permanent redirect

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(short))
         .bind(("0.0.0.0", 8080))?
         .run()
         .await
@@ -11,4 +11,11 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/short")]
+async fn short() -> impl Responder {
+    HttpResponse::PermanentRedirect()
+        .insert_header(("Location", "https://www.google.com"))
+        .body("")
 }
