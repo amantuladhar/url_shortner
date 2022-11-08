c668e132d6ca7d716df4de353ca278ff8b56edde
Aman Tuladhar <atuladhar.aman@gmail.com>
2022-11-07 21:32:59 -0600

    Try

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -6,5 +6,5 @@ touch /home/ubuntu/applogs/url_shortner.log
 
 # Run the application
 # export DATABASE_URL=sqlite:./test_123123123.db?mode=rwc
-export DATABASE_URL=test
+__DATABASE_URL__
 /home/ubuntu/url_shortner/url_shortner >> /home/ubuntu/applogs/url_shortner.log 2>&1 &
