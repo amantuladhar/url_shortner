66ad8054445a4d1df773ed541ba37411cc808c7b
Aman Tuladhar <atuladhar.aman@gmail.com>
2022-11-07 20:55:30 -0600

    Check if env variable works on script

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -5,4 +5,6 @@ mkdir -p /home/ubuntu/applogs
 touch /home/ubuntu/applogs/url_shortner.log
 
 # Run the application
+# export DATABASE_URL=sqlite:./test_123123123.db?mode=rwc
+export DATABASE_URL=test
 /home/ubuntu/url_shortner/url_shortner >> /home/ubuntu/applogs/url_shortner.log 2>&1 &
